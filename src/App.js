8025a80edf3bf3430949a738311c4abc53064872
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-02-17 12:35:23 +0100

    SearchBox komponens + state

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import TerminatorList from "./components/terminator-list/terminator-list.component";
+import { models } from "./models.js";
+import SearchBox from "./components/searchbox/searchbox.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      models: models,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredModels = this.state.models.filter((model) => {
+      return model.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>Terminator modellek</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <TerminatorList models={filteredModels} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "tachyons";
-import { models } from './models';
+import App from "./App";
 
-import TerminatorList from "./components/terminator-list/terminator-list.component";
-
-ReactDOM.render(
-  <TerminatorList models={models} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
