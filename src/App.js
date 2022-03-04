18e1adb0c0fabbdce09d9b6a3c544571015aab08
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-03-04 17:03:12 +0100

    API hívás használata lokális adatfájl helyett. / Using API instead of local datafile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import TerminatorList from "./components/terminator-list/terminator-list.component";
-import { models } from "./models.js";
+
 import SearchBox from "./components/searchbox/searchbox.component";
 
 import './App.css';
 
 class App extends Component {
   constructor() {
+
     super();
     this.state = {
-      models: models,
+      models: [],
       searchfield: "",
     };
   }
@@ -18,12 +19,26 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => this.setState({ models: users }));
+    
+
+  }
+
   render() {
+
     const filteredModels = this.state.models.filter((model) => {
       return model.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
+
+    if(this.state.models.length === 0) {
+      return <h1>Betöltés folyamatban...</h1>
+    }
+
     return (
       <div className="tc">
         <h1 className="f1">Terminator modellek</h1>
