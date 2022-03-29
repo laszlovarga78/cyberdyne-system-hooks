dadcc51fc696e9250f7aa1d5a99b1859bb639913
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-03-29 09:38:05 +0200

    Webalkalmazás átalakítása Hook-okkal történő működésre

--- README.md
+++ README.md
@@ -71,3 +71,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 # cyberdyne-system
 # cyberdyne-system-search-state-object
 # cyberdyne-system-api-call
+#cyberdyne-system-hooks
--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import TerminatorList from "./components/terminator-list/terminator-list.component";
 
 import SearchBox from "./components/searchbox/searchbox.component";
 
-import './App.css';
+import "./App.css";
 
-class App extends Component {
-  constructor() {
+function App() {
+  const [models, setModels] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-    super();
-    this.state = {
-      models: [],
-      searchfield: "",
-    };
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((users) => this.setState({ models: users }));
-    
+      .then((response) => response.json())
+      .then((users) => setModels(users));
+  }, []);
 
+  const filteredModels = models.filter((model) => {
+    return model.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  if (models.length === 0) {
+    return <h1>Betöltés folyamatban...</h1>;
   }
 
-  render() {
+  return (
+    <div className="tc">
+      <h1 className="f1">Terminator modellek</h1>
 
-    const filteredModels = this.state.models.filter((model) => {
-      return model.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
-    });
-
-    if(this.state.models.length === 0) {
-      return <h1>Betöltés folyamatban...</h1>
-    }
-
-    return (
-      <div className="tc">
-        <h1 className="f1">Terminator modellek</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <TerminatorList models={filteredModels} />
-      </div>
-    );
-  }
+      <SearchBox searchChange={onSearchChange} />
+      <TerminatorList models={filteredModels} />
+    </div>
+  );
 }
 
 export default App;
